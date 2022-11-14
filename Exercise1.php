fdcec8f04e7091b5859c6f4213e5bffd29a3a2ba
Sarah Martinez <112424028+ladyOouf@users.noreply.github.com>
2022-11-13 23:10:13 -0600

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "<table cellpadding = 5>";
+
+echo "<tr>";
+echo "<td>"." "."</td>";
+for($i=1; $i<=100; $i++)
+{
+	echo "<td>".$i."</td>";
+}
+echo "</tr>";
+
+
+for($i=1; $i<=100; $i++)
+{
+	echo "<tr>";
+	echo "<td>".$i."</td>";
+	for ($j=1; $j<=100; $j++)
+  	{
+  		echo "<td>".$i*$j."</td>";
+  	}
+  	echo "</tr>";
+}
+
+echo "</table>";
+?>
